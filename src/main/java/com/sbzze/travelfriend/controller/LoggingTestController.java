5bbb0e4c00e24bc55edf6bad72bdfc284b774d17
CodeJess <905333779@qq.com>
2020-05-10 12:36:45 +0800

    [ADD]日志工具模块

---
+++ src/main/java/com/sbzze/travelfriend/controller/LoggingTestController.java
@@ -0,0 +1,25 @@
+package com.sbzze.travelfriend.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/testlogging")
+public class LoggingTestController {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    @GetMapping("/hello")
+    public String hello() {
+        for (int i=0; i<100; i++) {
+            logger.info("info execute index method");
+            logger.warn("warn execute index method");
+            logger.error("error execute index method");
+        }
+        System.out.println(System.getProperty("user.home"));
+        return "My First SpringBoot Application";
+    }
+}
